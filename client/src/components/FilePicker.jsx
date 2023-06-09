e94834992663336ceb4af021143317bcc2e9b090
= <=>
2023-06-09 17:22:43 +0300

    sowking on editor tabs

---
+++ client/src/components/FilePicker.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import CustomButton from "./CustomButton";
+
+function FilePicker({ file, setFile, readFile }) {
+  return (
+    <div className="filepicker-container">
+      <div className="flex flex-1 flex-col">
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload File
+        </label>
+
+        <p className="mt-2 text-gray-500 text-xs truncate">
+          {file === "" ? "No File Selected" : file.name}
+        </p>
+      </div>
+
+      <div className="mt-4 flex flex-wrap gap-36">
+        <CustomButton type={"outline"} title={"Logo"} handleClick={() => readFile("logo")} customStyles={"text-xs"} />
+</div>
+      
+    </div>
+  );
+}
+
+export default FilePicker;
